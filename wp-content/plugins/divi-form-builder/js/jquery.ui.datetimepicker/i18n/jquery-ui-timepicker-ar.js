b92b5a487950c78c9c8e005120aa33630addb850
Adan Marcano <soporte.conoracle@gmail.com>
2025-10-04 21:08:18 -0500

    prueba2

---
+++ wp-content/plugins/divi-form-builder/js/jquery.ui.datetimepicker/i18n/jquery-ui-timepicker-ar.js
@@ -0,0 +1,22 @@
+/* Arabic translation for the jQuery Timepicker Addon */
+/* Written by Deon Heyns */
+(function($) {
+	$.timepicker.regional['ar'] = {
+		timeOnlyTitle: 'اختر الوقت',
+		timeText: 'زمن ',
+		hourText: 'ساعة ',
+		minuteText: 'دقيقة',
+		secondText: 'ثان',
+		millisecText: 'ميلي ثانية',
+		microsecText: 'ميكروثانية',
+		timezoneText: 'وحدة زمنية',
+		currentText: 'الآن',
+		closeText: 'فعله',
+		timeFormat: 'HH:mm',
+		timeSuffix: '',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		isRTL: true
+	};
+	$.timepicker.setDefaults($.timepicker.regional['ar']);
+})(jQuery);
---
+++ wp-content/plugins/divi-form-builder/js/jquery.ui.datetimepicker/i18n/jquery-ui-timepicker-nb.js
@@ -0,0 +1,21 @@
+/* English translation for the jQuery Timepicker Addon */
+(function($) {
+	$.timepicker.regional['nb'] = {
+		currentText: 'Now',
+		closeText: 'Done',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		timeFormat: 'HH:mm',
+		timeSuffix: '',
+		timeOnlyTitle: 'Choose Time',
+		timeText: 'Time',
+		hourText: 'Hour',
+		minuteText: 'Minute',
+		secondText: 'Second',
+		millisecText: 'Millisecond',
+		microsecText: 'Microsecond',
+		timezoneText: 'Time Zone',
+		isRTL: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['nb']);
+})(jQuery);
